4ad01728cab057e7b366d0563677ae1943a3964c
Lidya Ghilay <lidyahenok92gmail.com>
2024-04-03 12:26:05 +0300

    Assignment Complete

---
+++ index.js
@@ -0,0 +1,14 @@
+const divide = (a,b) =>(a/b)
+let a = 2000
+let b = 5
+console.log(divide);
+
+
+const square =(x) => (x*x)
+let x = 4
+console.log(square);
+
+const add = (w,y) => (w + y)
+let w = 4
+let y = 7
+console.log(add);
